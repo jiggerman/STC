807bfe88b0ad90f931a373575fa3384391ea8288
jiggerman <liggerman@mail.ru>
2024-02-18 01:32:14 +0300

    Add struct data_t

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cstddef>
+
+struct data_t
+{
+	size_t* ptr;
+	double size;
+	bool isError;
+};
 
 int main()
 {
   std::cout << "Hello, world!\n";
+  std::cout << sizeof(size_t);
   return 0;
 }
